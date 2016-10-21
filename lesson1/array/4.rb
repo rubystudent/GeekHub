fcf03d497ba7365ec11613160ec1c098a96eab58
rubystudent <cfgcl18@ukr.net>
2016-10-21 17:38:39 +0300

    StyleGuide improvements

--- lesson1/array/4.rb
+++ lesson1/array/4.rb
@@ -1,5 +1,5 @@
 array = []
-months = Hash.new{ |hash, key| hash[key] = []} 
+months = Hash.new { |hash, key| hash[key] = [] }
 40.times do
   day = rand(1..31)
   month = rand(1..12)
@@ -9,12 +9,12 @@ end
 
 array.each do |value|
   date, temp = value.split(' ')
-  day, month = date.split('.')
+  month = date.split('.')[1]
   months[month.to_i].push(temp.to_i)
 end
 
 months.sort.each do |key, value|
-  temp = value.inject(0){ |rezult, elem| rezult +elem} / value.size
-  temp = "Нет данных за этот месяц" if temp == 0
+  temp = value.inject(0) { |a, e| a + e } / value.size
+  temp = 'Нет данных за этот месяц' if temp.zero?
   puts "#{key} : #{temp}"
 end
